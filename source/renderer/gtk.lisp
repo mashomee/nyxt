65d6b7e1acf4c2a857ab7ed44836882c66c83793
Artyom Bologov <aartaka@protonmail.com>
2021-08-03 16:08:53 +0500

    Add fully-qualified IDs to extensions.

--- source/renderer/gtk.lisp
+++ source/renderer/gtk.lisp
@@ -963,6 +963,7 @@ See `gtk-browser's `modifier-translator' slot."
   (when extension
     `(("description" . ,(or (nyxt/web-extensions:description extension) ""))
       ("homepageUrl" . ,(or (nyxt/web-extensions:homepage-url extension) ""))
+      ("id" . ,(id extension))
       ("installType" . "development")
       ("mayDisable" . t)
       ("name" . ,(or (name extension) ""))
@@ -973,7 +974,6 @@ See `gtk-browser's `modifier-translator' slot."
       ("enabled" . t)
       ("hostPermissions" . ,(vector))
       ("icons" . ,(vector))
-      ("id" . 0)
       ("offlineEnabled" . nil)
       ("optionsUrl" . "")
       ("shortName" . ,(or (name extension) ""))
